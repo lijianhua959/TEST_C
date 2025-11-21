6eb144ee4a0e3c620b056ee65762e6730fc8e38a
Your Name <you@example.com>
2025-11-21 16:05:56 +0800

    新增部分打印内容

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;      
+    std::cout << "Hello, World!" << std::endl; 
+    
+    std::cout << "2025/11/21 15:58 在linux上做了些许修改!" << std::endl; 
 
     return 0;
 }
